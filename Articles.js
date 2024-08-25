f380c46ef77f92be83c935078a13604b7fa5a2f9
Lubna Sameh Mohamed <154529377+LubnaSameh@users.noreply.github.com>
2024-08-25 12:11:48 +0300

    Update Articles.js

--- Articles.js
+++ Articles.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './css/Articles.css';
+import './css/styleOfThreeTables.css';
 
 const Articles = () => {
     const articles = [
@@ -13,57 +13,70 @@ const Articles = () => {
 
     return (
         <div className="container mt-3">
-            <div className="row align-items-center justify-content-between mb-3">
-                <div className="col-md-5 text-md-start text-center">
-                    <div className="header-title">
-                        Articles
-                        <div className="linee mx-auto mx-md-0"></div>
+            <div className="row">
+                {/* Left Div */}
+                <div className="col-lg-4 col-md-12 mb-3">
+                    <div className="left-content mt-3">
+                        <p className="text-white">Here you can add the content you want to place on the left side.</p>
                     </div>
                 </div>
-                <div className="col-md-5 mt-3 mt-md-0">
-                    <div className="search-container">
-                        <input type="text" className="search-input" placeholder="Search In Jobs" />
-                        <button className="search-btn"><i className="bi bi-search"></i></button>
+                {/* Right Div */}
+                <div className="col-lg-8 col-md-12">
+                    {/* content of right div */}
+                    <div className="row align-items-center justify-content-between mb-3">
+                        <div className="col-md-5 text-md-start text-center">
+                            <div className="header-title">
+                                Articles
+                                <div className="linee mx-auto mx-md-0"></div>
+                            </div>
+                        </div>
+                        {/* searsh of right div */}
+                        <div className="col-md-5 mt-3 mt-md-0">
+                            <div className="search-container">
+                                <input type="text" className="search-input" placeholder="Search In Jobs" />
+                                <button className="search-btn"><i className="bi bi-search"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                    {/* table */}
+                    <div className="table-responsive">
+                        <table className="table table-dark articles-table">
+                            <thead>
+                                <tr>
+                                    <th>Title</th>
+                                    <th>Category</th>
+                                    <th>Status</th>
+                                    <th>Date & Time</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {articles.map((article, index) => (
+                                    <tr key={index}>
+                                        <td data-label="Title">{article.title}</td>
+                                        <td data-label="Category">{article.category}</td>
+                                        <td data-label="Status">
+                                            <span className={`btn ${article.status === 'Published' ? 'bg-primary' : 'bg-grey'}`}>
+                                                {article.status}
+                                            </span>
+                                        </td>
+                                        <td data-label="Date & Time">
+                                            <span className="date-time">
+                                                {`${article.dateTime.split(' ')[0]} ${article.dateTime.split(' ')[1]} ${article.dateTime.split(' ')[2]}`}<br />
+                                                {`${article.dateTime.split(' ')[3]} ${article.dateTime.split(' ')[4]}`}
+                                            </span>
+                                        </td>
+                                        <td className="text-end">
+                                            <div className="action-buttons">
+                                                <button className="me-1 btn-act"><i className="bi bi-pencil-square"></i></button>
+                                                <button className="btn-act"><i className="bi bi-trash-fill"></i></button>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
                     </div>
                 </div>
-            </div>
-
-            <div className="table-responsive">
-                <table className="table table-dark articles-table">
-                    <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Category</th>
-                            <th>Status</th>
-                            <th>Date & Time</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {articles.map((article, index) => (
-                            <tr key={index}>
-                                <td data-label="Title">{article.title}</td>
-                                <td data-label="Category">{article.category}</td>
-                                <td data-label="Status">
-                                    <span className={`btn ${article.status === 'Published' ? 'bg-primary' : 'bg-grey'}`}>
-                                        {article.status}
-                                    </span>
-                                </td>
-                                <td data-label="Date & Time">
-                                    <span className="date-time">
-                                        {`${article.dateTime.split(' ')[0]} ${article.dateTime.split(' ')[1]} ${article.dateTime.split(' ')[2]}`}<br />
-                                        {`${article.dateTime.split(' ')[3]} ${article.dateTime.split(' ')[4]}`}
-                                    </span>
-                                </td>
-                                <td className="text-end">
-                                    <div className="action-buttons">
-                                        <button className="me-1 btn-act"><i className="bi bi-pencil-square"></i></button>
-                                        <button className="btn-act"><i className="bi bi-trash-fill"></i></button>
-                                    </div>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
             </div>
         </div>
     );
