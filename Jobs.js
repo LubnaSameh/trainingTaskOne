c7ae73023041802ddfbbcf140463eb4eebd8c30b
Lubna Sameh Mohamed <154529377+LubnaSameh@users.noreply.github.com>
2024-08-26 19:30:14 +0300

    Update Jobs.js

--- Jobs.js
+++ Jobs.js
@@ -11,8 +11,21 @@ const Jobs = () => {
     ];
 
     return (
-        <div className="container mt-3">
+        <div className="container mt-3 mb-5">
             <div className="row">
+                {/* Top Div */}
+                <div className="top-bar mb-0 mb-md-5 d-flex justify-content-between align-items-center mb-3">
+                    <div className="top-info text-white mb-0 mb-md-5 ">
+                        <div className="admin-panel-title">
+                            Admin Panel
+                            <div className="lineP "></div>
+                        </div>
+                        <p className="panel-date mb-0 mb-md-5">6th June 2023</p>
+                    </div>
+                    <div className="d-none d-lg-block">
+                        <button className="btn btn-gold">Create New Job</button>
+                    </div>
+                </div>
                 {/* Left Div */}
                 <div className="col-lg-3 col-md-12 mb-3">
                     <div className="left-content mt-3">
@@ -32,10 +45,21 @@ const Jobs = () => {
                         </div>
                         {/* search of right div */}
                         <div className="col-md-5 mt-3 mt-md-0">
-                            <div className="search-container">
-                                <input type="text" className="search-input" placeholder="Search In Jobs" />
-                                <button className="search-btn"><i className="bi bi-search"></i></button>
+                            {/* Search Div for Desktop */}
+                            <div className="search-container d-none d-lg-flex">
+                                <input type="text" className="search-input" placeholder="Search In Users" />
+                                <button className="search-btn"><i className="fas fa-search"></i></button>
                             </div>
+                            {/* Menu for Mobile */}
+                            <div className="d-block d-lg-none">
+                                <div className="mobile-dropdown">
+                                    <button className="dropdown-btn">
+                                       Published Jobs
+                                        <i className="fas fa-chevron-down dropdown-arrow"></i> 
+                                    </button>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
                     {/* table */}
@@ -47,7 +71,7 @@ const Jobs = () => {
                                     <th>Position</th>
                                     <th>Status</th>
                                     <th>Posted At</th>
-                                    <th>#Applications</th>
+                                    <th>Applications</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -56,7 +80,7 @@ const Jobs = () => {
                                         <td data-label="Company">{job.company}</td>
                                         <td data-label="Position">{job.position}</td>
                                         <td data-label="Status">
-                                            <span className={`btn ${job.status === 'Open' ? 'bg-success' : 'bg-secondary'}`}>
+                                            <span className={`btn ${job.status === 'Open' ? 'bg-primary' : 'bg-secondary'}`}>
                                                 {job.status}
                                             </span>
                                         </td>
@@ -64,16 +88,15 @@ const Jobs = () => {
                                             <span className="date-time">
                                                 {`${job.dateTime.split(' ')[0]} ${job.dateTime.split(' ')[1]} ${job.dateTime.split(' ')[2]}`}<br />
                                                 {`${job.dateTime.split(' ')[3]} ${job.dateTime.split(' ')[4]}`}
+
                                             </span>
                                         </td>
 
-
-
-                                        <td data-label="# Applications">{job.applications}</td>
+                                        <td data-label="Applications" className=" text-start text-md-center">{job.applications}</td>
                                         <td className="text-end">
                                             <div className="action-buttons">
-                                                <button className="me-1 btn-act"><i className="bi bi-pencil-square"></i></button>
-                                                <button className="btn-act"><i className="bi bi-trash-fill"></i></button>
+                                                <button className="btn-act me-1 me-md-0 me-xl-1"><i className="fas fa-edit"></i></button>
+                                                <button className="btn-act"><i className="fas fa-trash-alt"></i></button>
                                             </div>
                                         </td>
                                     </tr>
@@ -82,28 +105,24 @@ const Jobs = () => {
                         </table>
                     </div>
 
-                    <div class="pagination">
-                        <a href="#" class="arrow">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon">
-                                <circle cx="12" cy="12" r="11" fill="none" stroke="#BF9530" stroke-width="2" />
-                                <path d="M14 16l-4-4 4-4" fill="none" stroke="#BF9530" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                            </svg>
+                    <div className="pagination">
+                        <a href="#" className="arrow">
+                            <i className="fas fa-chevron-left" style={{ color: '#BF9530' }}></i>
                         </a>
+
                         <a href="#">1</a>
                         <a href="#">2</a>
                         <a href="#">3</a>
                         <span>.....</span>
-
-                        <a href="#" class="arrow">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon">
-                                <circle cx="12" cy="12" r="11" fill="none" stroke="#BF9530" stroke-width="2" />
-                                <path d="M10 8l4 4-4 4" fill="none" stroke="#BF9530" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                            </svg>
+                        <a href="#" className="arrow">
+                            <i className="fas fa-chevron-right" style={{ color: '#BF9530' }}></i>
                         </a>
                     </div>
 
-
-
+                     {/* Button at the bottom */}
+                     <div className="d-block d-lg-none text-center ">
+                        <button className="btn btn-gold">Create New Job</button>
+                    </div>
                 </div>
             </div>
         </div>
